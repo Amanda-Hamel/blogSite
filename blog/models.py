e2f281eeb55bed1a1f99afb5ffff0adfa30a76e8
amanda-hamel <amanda.hamel@snhu.edu>
2020-12-17 19:34:01 -0500

    second commit

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 from django.db import models
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -13,3 +14,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self): # new
+        return reverse('post_detail', args=[str(self.id)])
